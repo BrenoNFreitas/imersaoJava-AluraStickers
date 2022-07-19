181e5216b0c96e66d2192f93239574c57cdeb74a
Breno <breno.freiitas.dn@gmail.com>
2022-07-19 20:50:22 -0300

    segunda aula

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -7,6 +9,11 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         //fazer uma conexão HTTP e buscar os top 250 filmes
         //String url = "https://imdb-api.com/en/API/Top250Movies/C H A V E(k_0ojt0yvm)"
@@ -23,10 +30,19 @@ public class App {
         System.out.println(listaDeFilmes.size());
 
         //exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            //System.out.println("Cartaz: " + filme.get("image"));
+            System.out.println("Título: " + titulo);
+            //ystem.out.println("Nota: "+ filme.get("imDbRating"));
             System.out.println();
         }
 
